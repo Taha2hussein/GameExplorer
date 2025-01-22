a35845a4697b53572f5abf561dc65cf0132f6dd3
Taha Hussein <taha.hussein.two@gmail.com>
2025-01-22 16:34:44 +0200

    Initial Commit

---
+++ GameExplorer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GameExplorer
+//
+//  Created by Taha Hussein on 22/01/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GameExplorer/GameExplorerApp.swift
@@ -0,0 +1,17 @@
+//
+//  GameExplorerApp.swift
+//  GameExplorer
+//
+//  Created by Taha Hussein on 22/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct GameExplorerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
